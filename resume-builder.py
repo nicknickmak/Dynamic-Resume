1f1d5f2267f38a6634cbcacdf35b515de2469ef0
Jenna Yang <jmy001@ucsd.edu>
2025-06-04 13:31:54 -0400

    move code to functions

--- resume-builder.py
+++ resume-builder.py
@@ -1,16 +1,26 @@
 import yaml
-from jinja2 import Environment, FileSystemLoader
-
-# Load YAML data
-with open("dynamic-resume.yml", "r") as file:
-    data = yaml.safe_load(file)
-
+"""
+Loads a YAML file of experiences
+@param path - path name to YAML file of experiences
+"""
+def load_experience_yaml(path):
+    with open(path, "r") as file:
+        return yaml.safe_load(file)
+    
+"""
+Returns the loaded the Job Description txt file
+@param path - path name to txt file of the job description
+"""
+def load_jd_text(path):
+    with open(path, "r") as file:
+        return file.read()
 
 # Set up Jinja2 environment
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template('resume_template.html')
 
 # Render the template with YAML data
+data = load_experience_yaml("full_resume.yaml")
 output = template.render(data)
 
 # Save the generated HTML
